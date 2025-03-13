a2df7523564acdfcd9ecbedf3258d11bf1e03d39
MANU0519 <anishkushwaha1116@gmail.com>
2025-03-13 10:36:01 +0545

    refactor: update footer copyright text to reflect project name

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
       <footer className="footer sm:footer-horizontal items-center p-4">
         <aside className="grid-flow-col items-center">
           <div className=" bg-cover bg-no-repeat size-28 bg-[url(/Logo.png)]"></div>
-          <p>Copyright © {new Date().getFullYear()} - All right reserved</p>
+          <p>Nanglo for PLEASE Hack</p>
         </aside>
         <nav className="grid-flow-col gap-4 md:place-self-center md:justify-self-end">
           <a>
