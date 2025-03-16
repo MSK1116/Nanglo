5d22b4d2ace3b5ded957428899a8499bc196b760
MANU0519 <anishkushwaha1116@gmail.com>
2025-03-16 19:47:17 +0545

    feat: add modal dialog to Home_banner for underdevelopment notice; remove modal from Partner_Home

--- frontend/src/partner/Partner_Home.jsx
+++ frontend/src/partner/Partner_Home.jsx
@@ -5,24 +5,12 @@ import Partner_Flashsale from "./Partner_Flashsale";
 
 const Partner_Home = () => {
   document.title = "Darazi | Online shopping in Sri Lanka";
-  useEffect(() => {
-    document.getElementById("my_modal_2").showModal();
-  }, []);
+
   return (
     <>
       <Partner_navbar />
       <Partner_offer />
       <Partner_Flashsale />
-
-      <dialog id="my_modal_2" className="modal">
-        <div className="modal-box">
-          <h3 className="font-bold text-lg">Caution!</h3>
-          <p className="py-4">This website is underdevelopment</p>
-        </div>
-        <form method="dialog" className="modal-backdrop">
-          <button>close</button>
-        </form>
-      </dialog>
     </>
   );
 };
