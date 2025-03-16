b0b374e8528698209040be38744f6f2ac85883ec
MANU0519 <anishkushwaha1116@gmail.com>
2025-03-16 19:45:21 +0545

    feat: add modal dialog to Partner_Home for underdevelopment notice; implement useEffect for automatic display

--- frontend/src/partner/Partner_Home.jsx
+++ frontend/src/partner/Partner_Home.jsx
@@ -1,15 +1,28 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Partner_navbar from "./Partner_navbar";
 import Partner_offer from "./Partner_offer";
 import Partner_Flashsale from "./Partner_Flashsale";
 
 const Partner_Home = () => {
   document.title = "Darazi | Online shopping in Sri Lanka";
+  useEffect(() => {
+    document.getElementById("my_modal_2").showModal();
+  }, []);
   return (
     <>
       <Partner_navbar />
       <Partner_offer />
       <Partner_Flashsale />
+
+      <dialog id="my_modal_2" className="modal">
+        <div className="modal-box">
+          <h3 className="font-bold text-lg">Caution!</h3>
+          <p className="py-4">This website is underdevelopment</p>
+        </div>
+        <form method="dialog" className="modal-backdrop">
+          <button>close</button>
+        </form>
+      </dialog>
     </>
   );
 };
