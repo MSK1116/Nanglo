c9aa9b7899d200a00925886cc7d72f4a4c4a51be
MANU0519 <anishkushwaha1116@gmail.com>
2025-03-18 17:40:10 +0545

    feat: add loading state to video component for better user experience

--- frontend/src/partner/Partner_read.jsx
+++ frontend/src/partner/Partner_read.jsx
@@ -6,7 +6,9 @@ import { FaPause } from "react-icons/fa";
 
 const Partner_read = () => {
   const videoRef = useRef(null);
+
   const [isPlaying, setIsPlaying] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
 
   const [vidProgress, setVidProgress] = useState(0);
 
@@ -29,16 +31,32 @@ const Partner_read = () => {
       <Navbar />
       <div className=" max-w-screen-2xl min-h-screen overflow-hidden">
         <div className=" flex w-full h-[95vh] relative mt-8">
-          <video onTimeUpdate={updateProgress} ref={videoRef} className=" w-full h-full object-cover" autoPlay loop muted playsInline>
-            <source src="/Video/Short_vid.mp4" type="video/mp4"></source>
-            Your browser does not support the video tag.
-          </video>
-          <div className="absolute bottom-2 left-10">
-            <button onClick={togglePlayPause} className={` overflow-hidden relative  px-4 py-2 opacity-80 bg-gray-300 text-gray-50 rounded-md `}>
-              {!isPlaying ? <FaPlay /> : <FaPause />}
-              <div className={` ${!isPlaying ? " bg-red-600" : "bg-emerald-600"} h-[2px] absolute bottom-0 left-0  rounded-lg "`} style={{ width: `${vidProgress}%` }}></div>
-            </button>
-          </div>
+          {isLoading ? (
+            <div className=" w-full h-full bg-red-700">Loading</div>
+          ) : (
+            <>
+              <video
+                onCanPlay={() => setIsLoading(false)}
+                onLoadedData={() => setIsLoading(false)}
+                onWaiting={() => setIsLoading(true)}
+                onTimeUpdate={updateProgress}
+                ref={videoRef}
+                className=" w-full h-full object-cover"
+                autoPlay
+                loop
+                muted
+                playsInline>
+                <source src="/Video/Short_vid.mp4" type="video/mp4"></source>
+                Your browser does not support the video tag.
+              </video>
+              <div className="absolute bottom-2 left-10">
+                <button onClick={togglePlayPause} className={` overflow-hidden relative  px-4 py-2 opacity-80 bg-gray-300 text-gray-50 rounded-md `}>
+                  {!isPlaying ? <FaPlay /> : <FaPause />}
+                  <div className={` ${!isPlaying ? " bg-red-600" : "bg-emerald-600"} h-[2px] absolute bottom-0 left-0  rounded-lg "`} style={{ width: `${vidProgress}%` }}></div>
+                </button>
+              </div>
+            </>
+          )}
         </div>
       </div>
       <Footer />
